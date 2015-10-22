faf3c1aa4e8847d1bcc981ea68306c71d8f05f0e
zarol <saharathkleips@yahoo.com>
2015-10-22 16:55:52 -0700

    added application object

---
+++ sim_02/application.cpp
@@ -0,0 +1,169 @@
+#include "application.h"
+
+Application::Application( Config config, int appID, 
+    std::list<std::string> operations )
+    :   m_config( config ), m_appID( appID ), m_operations( operations )
+{
+    calculateApplicationTime();
+}
+
+/**
+ * @brief      Executes each process "P", input "I", and output "O" linearly.
+ * @details    Each I/O operation executes within its own thread.
+ */
+void Application::ApplicationLoop()
+{
+    std::cout << Timer::msDT() << " - OS: START process " << m_appID 
+        << std::endl;
+
+    // Loop through the operations and "execute" each one
+    for( std::list<std::string>::iterator iter = m_operations.begin();
+         iter != m_operations.end(); ++iter )
+    {
+        std::map<std::string, std::string> operation = Parser::splitOperation( 
+            *iter );
+        int runningTime = calculateOperationTime( operation );
+
+        if( operation["Component"] == "P" )
+        {
+            runProcess( runningTime );
+        }
+        else if( operation["Component"] == "I" )
+        {
+            std::thread inputThread( &Application::runInput, this, 
+                operation["Operation"], runningTime );
+            inputThread.join();
+        }
+        else if( operation["Component"] == "O" )
+        {
+            std::thread outputThread( &Application::runOutput, this, 
+                operation["Operation"], runningTime );
+            outputThread.join();
+        }
+        if( iter != m_operations.begin() )
+            m_operations.erase( iter-- );
+    }
+    m_operations.erase( m_operations.begin() );
+
+    std::cout << Timer::msDT() << " - OS: END process " << m_appID 
+        << std::endl;
+}
+
+//
+// I/O/P ACTIONS ///////////////////////////////////////////////////////////////
+//
+
+/**
+ * @brief      Executes a process "P" with a defined wait time.
+ *
+ * @param[in]  cycleTime  The time in miliseconds to simulate an operation by
+ * sleeping the thread.
+ */
+void Application::runProcess( int runningTime )
+{
+    std::cout << Timer::msDT() << " - Process " << m_appID 
+        << ": START processing action" << std::endl;
+
+    std::this_thread::sleep_for( std::chrono::milliseconds( runningTime ) );
+
+    std::cout << Timer::msDT() << " - Process " << m_appID 
+        << ": END processing action" << std::endl;
+}
+
+/**
+ * @brief      Executes an input operation "I" with a defined wait time and a
+ * defined operation name.
+ *
+ * @note       This function is currently very similar to runOutput, but is kept
+ * separately in case future iterations of the simulation causes them to change
+ * more distinctly.
+ * 
+ * @param[in]  name       The name of the operation. It may be "hard drive",
+ * "keyboard", "monitor", or "printer".
+ * @param[in]  cycleTime  The time in miliseconds to simulate an operation by
+ * sleeping the thread.
+ */
+void Application::runInput( std::string name, int runningTime )
+{
+    std::cout << Timer::msDT() << " - Process " << m_appID << ": START " << 
+        name << " input" << std::endl;
+
+    std::this_thread::sleep_for( std::chrono::milliseconds( runningTime ) );
+
+    std::cout << Timer::msDT() << " - Process " << m_appID << ": END " << 
+        name << " input" << std::endl;
+}
+
+/**
+ * @brief      Executes an output operation "O" with a defined wait time and a
+ * defined operation name.
+ * 
+ * @note       This function is currently very similar to runInput, but is kept
+ * separately in case future iterations of the simulation causes them to change
+ * more distinctly.
+ *
+ * @param[in]  name       The name of the operation. It may be "hard drive",
+ * "keyboard", "monitor", or "printer".
+ * @param[in]  cycleTime  The time in miliseconds to simulate an operation by
+ * sleeping the thread.
+ */
+void Application::runOutput( std::string name, int runningTime )
+{
+    std::cout << Timer::msDT() << " - Process " << m_appID << ": START " << 
+        name << " output" << std::endl;
+
+    std::this_thread::sleep_for( std::chrono::milliseconds( runningTime ) );
+
+    std::cout << Timer::msDT() << " - Process " << m_appID << ": END " << 
+        name << " output" << std::endl;
+}
+
+//
+// HELPER FUNCTIONS ////////////////////////////////////////////////////////////
+//
+
+
+void Application::calculateApplicationTime()
+{
+    ApplicationTime = 0;
+    for( std::list<std::string>::const_iterator iter = m_operations.begin();
+         iter != m_operations.end(); ++iter )
+    {
+        std::map<std::string, std::string> operation = Parser::splitOperation( 
+            *iter );
+        ApplicationTime += calculateOperationTime( operation );
+    }
+}
+
+int Application::calculateOperationTime( 
+    std::map<std::string, std::string> operation )
+{
+    int cycleTime = std::stoi( operation["Cycle"] );
+
+    if( operation["Component"] == "P" )
+    {
+        return cycleTime * m_config.processorCycle;
+    }
+    else if( operation["Component"] == "I" ||
+             operation["Component"] == "O")
+    {
+        if( operation["Operation"] == "hard drive" )
+        {
+            return cycleTime * m_config.hardDriveCycle;
+        }
+        else if ( operation["Operation"] == "keyboard" )
+        {
+            return cycleTime * m_config.keyboardCycle;
+        }
+        else if ( operation["Operation"] == "monitor" )
+        {
+            return cycleTime * m_config.monitorDisplayCycle;
+        }
+        else if ( operation["Operation"] == "printer" )
+        {
+            return cycleTime * m_config.printerCycle;
+        }
+    }
+        
+    return 0;
+}
---
+++ sim_02/application.h
@@ -0,0 +1,35 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include <list>
+#include <map>
+#include <thread>
+#include <chrono>
+
+#include "config.h"
+#include "parser.h"
+#include "timer.h"
+
+class Application
+{
+public:
+    Application( Config config, int appID, 
+        std::list<std::string> operations );
+
+    void ApplicationLoop();
+
+    int ApplicationTime;
+private:
+    void runProcess( int cycleTime );
+    void runInput( std::string name, int cycleTime );
+    void runOutput( std::string name, int cycleTime );
+
+    void calculateApplicationTime();
+    int calculateOperationTime( std::map<std::string, std::string> operation );
+
+    Config m_config;
+    int m_appID;
+    std::list<std::string> m_operations;
+};
+
+#endif  //  APPLICATION_H
