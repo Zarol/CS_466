bbc8f9714f1248dc1ea3ec967233ae572a0564db
zarol <saharathkleips@yahoo.com>
2015-10-22 18:32:54 -0700

    did something

--- sim_02/application.h
+++ sim_02/application.h
@@ -1,7 +1,6 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-#include <iostream>
 #include <list>
 #include <map>
 #include <thread>
@@ -14,8 +13,7 @@
 class Application
 {
 public:
-    Application( Config config, int appID, 
-        std::list<std::string> operations );
+    Application( Config config, int appID, std::list<std::string> operations );
 
     void ApplicationLoop();
 
@@ -31,9 +29,6 @@ private:
     Config m_config;
     int m_appID;
     std::list<std::string> m_operations;
-
-    friend bool operator<( const Application& app1, const Application& app2 );
-    friend std::ostream& operator<<( std::ostream& os, const Application& app );  
 };
 
 #endif  //  APPLICATION_H
